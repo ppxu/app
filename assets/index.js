fc8190b10e9b755e3866e3932d6dec175c150789
zihan <zihan.yx@taobao.com>
2013-05-15 23:19:21 +0800

    update

--- assets/index.js
+++ assets/index.js
@@ -1,121 +1,155 @@
-KISSY.add('reader', function(S, DOM, Event, UA, IO) {
+KISSY.add('reader', function (S, DOM, Event, UA, IO) {
 
-    var MC = S.mix({}, S.EventTarget); // Message Center
+    var MC = S.mix({}, S.EventTarget);  // Message Center
 
 
     var JSONP = 'jsonp';
 
 
     var entryCache = {},
-    listCache = {};
+        listCache = {};
 
     var catList = DOM.get('#user-cat-list'),
         entryList = DOM.get('#list'),
         entryContent = DOM.get('#entry-content');
 
+    var UA = KISSY.UA;
 
-    var renderList = function(list) {
-        currentList = list;
-
-        var entries = [];
-        S.each(list, function(entryInfo) {
-            var entry = '<li class="title level-1 {clazz}">' +
-                '<div class="inner">' +
-                '<h2><a href="{link}" data-id={id}>{title}</a></h2>' +
-                '<p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</p>' +
-                '</div>' +
-                '<span class="mark-read"><i class="icon-ok"></i></span>' +
-                '<span class="mark-star"><i class="icon-heart"></i></span>' +
-                '</li>';
-
-            entryInfo['clazz'] = entryInfo['is_unread'] ? 'unread' : 'read';
-            entry = S.substitute(entry, entryInfo);
-            entries.push(entry);
-
-        });
-
-        entries = entries.join('');
-
-        DOM.html('.entry-list', entries);
-
-    },
+    var isDesktop = !UA.ios;
 
 
-    renderEntry = function(id) {
-        if (entryCache[id]) {
-            _renderEntry(entryCache[id])
-        } else {
-            IO.get('http://10.232.133.213/item/' + id, function(entry) {
-                entryCache[id] = entry;
-                _renderEntry(entry);
-            }, JSONP);
-        }
-    },
+    var renderList = function (list) {
+            currentList = list;
 
-    renderCatList = function(list) {
+            var entries = [];
+            S.each(list, function (entryInfo) {
+                            var entry = '<li class="title level-1 {clazz}">' +
+                                '<div class="inner">' +
+                                '<h2><a href="{link}" data-id={id}>{title}</a></h2>' +
+                                '<p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</p>' +
+                                '</div>' +
+                                '<span class="mark-read"><i class="icon-ok"></i></span>' +
+                                '<span class="mark-star"><i class="icon-heart"></i></span>' +
+                                 '</li>';
 
-        var cats = [];
-        S.each(list, function(catInfo) {
+                entryInfo['clazz'] = entryInfo['is_unread'] ? 'unread' : 'read';
+                entry = S.substitute(entry, entryInfo);
+                entries.push(entry);
 
-            var cat = '<li class="list-detail title level-1 {clazz}"><h2>' +
-                '<a href="{link}" data-id={id}>{title}</a>' +
-                '</h2>' +
-                '</li>';
+            });
+            entries = entries.join('');
+            //gaozhan  begin
+            var listcontent= '<div class="entries section">'+
+                ' <div class="scroll_content"  style="overflow:auto;">'+
+                '<ul class="entry-list" data-bind="template: { name: "tpl-entry-head", foreach: entryList }">'+
+                        '<!--         <script type="text/html" id="tpl-entry-head">'+
+                           ' <li>'+
+                                '<h2 data-bind="text:title">'+
+                                '</h2>'+
+                            '</li>'+
+                       ' </script>-->'+
+                    ' </ul>'+
+                ' </div>'+
+            '</div>';
+            DOM.html('#list .entries', listcontent);
+            //gaozhna  end
+            DOM.html('.entry-list', entries);
+            //gaozhan  begin
+            if(S.one(".entry-list").innerHeight()>DOM.viewportHeight() && isDesktop){
+                S.use("scroll,dom", function (S, Kscroll,DOM) {
+                    var $ = S.all,
+                        d = new Kscroll($(".scroll_content"), {
+                            prefix:"clear-",
+                            hotkey: true,
+                            bodydrag: true,
+                            allowArrow:true
+                        });
+                });
+            }
+            DOM.style('.scroll_content',{"height":DOM.viewportHeight()-60});
+            //gaozhan  end
+        },
 
-            catInfo['clazz'] = catInfo['is_unread'] ? 'unread' : 'read';
-            cat = S.substitute(cat, catInfo);
-            cats.push(cat);
 
-        });
+        renderEntry = function (id) {
+            if (entryCache[id]) {
+                _renderEntry(entryCache[id])
+            } else {
+                IO.get('http://10.232.133.213/item/' + id, function (entry) {
+                    entryCache[id] = entry;
+                    _renderEntry(entry);
+                }, JSONP);
+            }
+        },
 
-        cats = cats.join('');
-        DOM.html(catList, cats);
+        renderCatList = function (list) {
 
-    },
+            var cats = [];
+            S.each(list, function (catInfo) {
 
-    _renderEntry = function(entry) {
-        DOM.html(entryContent, entry.description)
-    },
+                var cat = '<li class="title list-detail level-1 {clazz}"><h2>' +
+                          '<a href="{link}" data-id={id}>{title}</a>' +
+                          '</h2>' +
+                          '</li>';
+
+                catInfo['clazz'] = catInfo['is_unread'] ? 'unread' : 'read';
+                cat = S.substitute(cat, catInfo);
+                cats.push(cat);
+
+            });
+
+            cats = cats.join('');
+            DOM.html(catList, cats);
+
+        },
+
+        _renderEntry = function (entry) {
+            //gaozhan
+            var entryDescription="<h1 class='title'>"+entry.title+"</h1><p class='time'>"+entry.pub_date+"</p><div class='content'>"+entry.description+"</div>";
+            DOM.html(entryContent, entryDescription);
+            //gaozhan
+        },
 
 
-    currentList = [], // current entry list
+        currentList = [],     // current entry list
 
 
-        // fetch entry list, including title/link
-        fetchList = function(id) {
+    // fetch entry list, including title/link
+        fetchList = function (id) {
             if (listCache[id]) {
                 renderList(listCache[id]);
-            } else IO.get('http://10.232.133.213/channel/' + id, function(list) {
+            } else
+            IO.get('http://10.232.133.213/channel/'+id,function(list){
                 renderList(list);
                 listCache[id] = list;
 
-            }, JSONP);
+            },JSONP);
 
         },
 
 
-        // toggle entry type in view
-        toggleView = function() {
+    // toggle entry type in view
+        toggleView = function () {
 
         },
 
 
-        setAll = function() {
+        setAll = function () {
 
         };
 
 
     // handle list clicks
-    Event.on(entryList, 'click', function(evt) {
-        console.log('title click');
+    Event.on(entryList, 'click', function (evt) {
+
         evt.preventDefault();
 
         var target = evt.target,
             id;
-
+        S.one(entryList).all("li").removeClass("current");//gaozhan
         if (target.tagName === 'A') {
-
             id = DOM.attr(target, 'data-id');
+            DOM.addClass(S.one(DOM.parent(target,".title")),"current");//gaozhan
             MC.fire('entry-list:click', {
                 id: id
             });
@@ -123,20 +157,20 @@ KISSY.add('reader', function(S, DOM, Event, UA, IO) {
 
     });
 
-    MC.on('entry-list:click', function(evt) {
+    MC.on('entry-list:click', function (evt) {
         var entryId = evt.id;
         renderEntry(entryId);
     });
 
 
-    Event.on(catList, 'click', function(evt) {
-        console.log('list click');
+    Event.on(catList, 'click', function (evt) {
         evt.preventDefault();
         var target = evt.target,
             id;
+        S.one(catList).all("li").removeClass("current");//gaozhan
         if (target.tagName === 'A') {
-
             id = DOM.attr(target, 'data-id');
+            DOM.addClass(S.one(DOM.parent(target,".title")),"current");//gaozhan
             MC.fire('cat-list:click', {
                 id: id
             });
@@ -146,37 +180,25 @@ KISSY.add('reader', function(S, DOM, Event, UA, IO) {
     });
 
 
-    MC.on('cat-list:click', function(evt) {
+    MC.on('cat-list:click', function (evt) {
         var id = evt.id;
         fetchList(id);
     });
 
-    IO.get('http://10.232.133.213/items/', function(defaultList) {
+    IO.get('http://10.232.133.213/items/', function (defaultList) {
         renderList(defaultList);
 
     }, JSONP);
-
-    IO.get('http://10.232.133.213/tags/', function(cats) {
+    IO.get('http://10.232.133.213/tags/', function (cats) {
         var cats = cats.data;
         renderCatList(cats);
-        MC.fire('home-loaded');
     }, JSONP);
 
+}, { requires: [
+    'dom',
+    'event',
+    'ua',
+    'ajax',
+    'xtemplate'
 
-    MC.on('home-loaded', function() {
-        if (S.DOM.viewportWidth() <= 480) {
-            S.use('mobile');
-        }
-    });
-
-
-}, {
-    requires: [
-        'dom',
-        'event',
-        'ua',
-        'ajax',
-        'xtemplate'
-
-    ]
-});
+]});
---
+++ assets/pad.js
@@ -0,0 +1,89 @@
+KISSY.add('pad', function (S, DOM, Event) {
+    //pad添加
+    var DOM = S.DOM, Event = S.Event;
+    var height = DOM.viewportHeight(),
+        tmp = DOM.offset('#content').top + parseInt(DOM.css('.column-actions','height').slice(0,-2)); console.log(tmp);
+    DOM.css('.sub-entry','height',height-tmp+'px');
+
+    //window resize
+    Event.on(window,'resize',function(e){
+        var height = DOM.viewportHeight(),
+            tmp = DOM.offset('#content').top + parseInt(DOM.css('.column-actions','height').slice(0,-2)); console.log(tmp);
+        DOM.css('.sub-entry','height',height-tmp+'px');
+    });
+
+    //todo swipe和hover冲突
+    Event.on('#list,#entry','swipe',function(e){
+        if(e.direction=="up"||e.direction=="down"){
+            return;
+        }
+        var lis = DOM.query('li','#cats'),
+            len = lis.length;
+        if(DOM.css('#cats','display')=="none"){
+            //显示第一屏
+
+            DOM.css('#entry','width','0');
+            DOM.css('#list','width','64%');
+
+            S.later(function(){
+                DOM.css('#cats','display','block');
+            },500);
+
+            for(var tmp = 0;tmp<len/2;tmp++){
+                S.later(function(tmp){
+                    DOM.css(lis[tmp],'transform','perspective(500px) rotateY(0deg)');
+                    DOM.css(lis[len-1-tmp],'transform','perspective(500px) rotateY(0deg)');
+                    DOM.css(lis[tmp],'-webkit-transform','perspective(500px) rotateY(0deg)');
+                    DOM.css(lis[len-1-tmp],'-webkit-transform','perspective(500px) rotateY(0deg)');
+                },500+100*tmp,false,null,tmp);
+            }
+
+        }else{
+            //隐藏第一屏
+            for(var tmp = 0;tmp<len/2;tmp++){
+                S.later(function(tmp){
+                    DOM.css(lis[tmp],'transform','perspective(500px) rotateY(90deg)');
+                    DOM.css(lis[len-1-tmp],'transform','perspective(500px) rotateY(90deg)');
+                    DOM.css(lis[tmp],'-webkit-transform','perspective(500px) rotateY(90deg)');
+                    DOM.css(lis[len-1-tmp],'-webkit-transform','perspective(500px) rotateY(90deg)');
+                },100*tmp,false,null,tmp);
+            }
+            S.later(function(){
+                DOM.css('#cats','display','none');
+                DOM.css("#list",'transition-duration',0.1*len/2+'s');
+                DOM.css("#list",'-webkit-transition-duration',0.1*len/2+'s');
+                DOM.css('#list','width','30%');
+
+                DOM.css("#entry",'transition-duration',0.1*len/2+'s');
+                DOM.css("#entry",'-webkit-transition-duration',0.1*len/2+'s');
+                DOM.css('#entry','width','69%');
+                DOM.css('#entry','left','0');
+                DOM.css('#entry','top','0');
+            },100*len/2);
+        }
+    });
+
+    Event.delegate('#list','click','.title',function(e){
+        if(DOM.css('#cats','display')=='block'){
+            Event.fire('#list','swipe');   //todo 内容加载不出来的时候会卡住，预设内容？
+        }
+        DOM.removeClass('.title','cur-title');
+        DOM.addClass(e.currentTarget,'cur-title');
+    });
+
+    Event.on('.icon-remove-sign','click',function(){
+        DOM.val('input','');
+    });
+
+
+    Event.on('.action-list','click','.action',function(e){
+        var target = e.currentTaregt,
+            i = DOM.get('i',target);
+       //todo 变化icon
+
+    })
+}, { requires: [
+    'dom',
+    'event'
+]});
+
